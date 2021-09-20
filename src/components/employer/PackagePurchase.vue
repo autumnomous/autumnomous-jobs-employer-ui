fbe054150edbed216aa6ad659d65e35d836f4557
Eli Autumn Sanders <easanders@pm.me>
2021-09-20 00:24:29 -0400

    fetch job packages from api

--- src/components/employer/PackagePurchase.vue
+++ src/components/employer/PackagePurchase.vue
@@ -1,11 +1,11 @@
 <template>
 
-    <div class="form-group" v-for="jobPackage in jobPackages" :key="jobPackage.title">
+    <div class="form-group" v-for="jobPackage in jobPackages" :key="jobPackage.id">
         <div class="form-control mb-2" >
         <div class="custom-control custom-radio" >
             
-            <input type="radio" class="custom-control-input" v-model="chosen"  name="job-package" :value="jobPackage.title" :id="jobPackage.title" >
-            <label class="custom-control-label" :for="jobPackage.title">{{jobPackage.title}} <span class="text-muted font-size-1">{{jobPackage.subtitle}}</span></label>
+            <input type="radio" class="custom-control-input" v-model="chosen"  name="job-package" :value="jobPackage.typeid" :id="jobPackage.title" >
+            <label class="custom-control-label" :for="jobPackage.title">{{jobPackage.title}} <span class="text-muted font-size-1">{{jobPackage.description}}</span></label>
             
         </div>
         </div>
@@ -17,19 +17,33 @@
 export default {
     data(){
         return{
-           jobPackages:[
-                  {title:"Single Job Posting", subtitle:"$150.00 for 1 job listed for 30 days"},
-                  {title:"5 Jobs Posting Package", subtitle:"$650.00 for 5 jobs listed for 30 days"},
-                  {title:"10 Jobs Posting Package", subtitle:"$1,250.00 for 10 jobs listed for 30 days"},
-                  {title:"Single Jobs Post + Social Media Blast", subtitle:"$450.00 for 1 job listed for 30 days"},
-                  {title:"5 Jobs Posting + Social Media Blasts Package", subtitle:"$1,950.00 for 5 jobs listed for 30 days"},
-                  {title:"10 Jobs Posting + Social Media Blasts Package", subtitle:"$3,800.00 for 10 jobs listed for 30 days"},
-                  {title:"Premium Job Ad Placement", subtitle:"$600 per job ad for 30 days"}
-                ],
-                chosen:"Single Job Posting",
+           jobPackages:[],
+            chosen:"Single Job Posting",
         }
     },
-    validations(){
+    async created(){
+
+        this.jobPackages = await fetch(process.env.VUE_APP_BIT_API_PATH + "/employer/get/jobpackages/active",
+                      {
+                          method: "GET",
+                          headers: {
+                              "Content-Type": "application/json",
+                              Authorization: "Bearer " + process.env.VUE_APP_JWT
+                          },
+                          credentials: "include"
+                      }
+                  ).then(result =>{
+
+                      if(!result.ok){
+                        console.log(result)
+                        return result
+                      }
+
+                      return result.json()
+
+                  })
+
+        this.jobPackages = this.jobPackages.sort((a,b) => a.id - b.id)
 
     }
 }
