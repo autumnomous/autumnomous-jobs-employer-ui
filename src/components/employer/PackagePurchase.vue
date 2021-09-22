b4d9dd4e78e56620d9d85cec5cff2fb106a4f287
Eli Autumn Sanders <easanders@pm.me>
2021-09-22 16:52:11 -0400

    go back to using api key

--- src/components/employer/PackagePurchase.vue
+++ src/components/employer/PackagePurchase.vue
@@ -28,7 +28,7 @@ export default {
                           method: "GET",
                           headers: {
                               "Content-Type": "application/json",
-                              Authorization: "Bearer " + process.env.VUE_APP_JWT
+                              Authorization: "Bearer " + process.env.VUE_APP_BIT_API_KEY
                           },
                         //   credentials: "include"
                       }
