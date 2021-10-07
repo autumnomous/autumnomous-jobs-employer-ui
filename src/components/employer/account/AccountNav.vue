56c82e85b57f43105c56ab20244fa33a3ab79fc5
Eli Autumn Sanders <easanders@pm.me>
2021-10-06 20:50:06 -0400

    working on account view for employers

---
+++ src/components/employer/account/AccountNav.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="col-lg-3">
+        <!-- Navbar -->
+        <div class="navbar-expand-lg navbar-expand-lg-collapse-block navbar-light">
+            <div id="sidebarNav" class="collapse navbar-collapse navbar-vertical">
+            <!-- Card -->
+            <div class="card mb-5">
+                <div class="card-body">
+                <!-- Avatar -->
+                <div class="d-none d-lg-block text-center mb-5">
+                    <h4 class="card-title">Natalie Curtis</h4>
+                    <p class="card-text font-size-1">natalie@example.com</p>
+                </div>
+                <!-- End Avatar -->
+
+                <!-- Nav -->
+                <h6 class="text-cap small">Account</h6>
+
+                <!-- List -->
+                <ul class="nav nav-sub nav-sm nav-tabs nav-list-y-2 mb-4">
+                <li class="nav-item">
+                    <a class="nav-link" @click='changeActive("the-basic-info")' :class="{'active': isActive=='the-basic-info'}">
+                    <i class="fas fa-id-card nav-icon"></i> Basic info
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" @click='changeActive("the-login-security")' :class="{'active': isActive=='the-login-security'}">
+                    <i class="fas fa-shield-alt nav-icon"></i> Login &amp; security
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="account-notifications.html">
+                    <i class="fas fa-bell nav-icon"></i> Notifications
+                    <span class="badge badge-soft-dark badge-pill nav-link-badge">1</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="account-preferences.html">
+                    <i class="fas fa-sliders-h nav-icon"></i> Preferences
+                    </a>
+                </li>
+            </ul>
+        <!-- End List -->
+
+        <h6 class="text-cap small">Company Information</h6>
+
+        <!-- List -->
+        <ul class="nav nav-sub nav-sm nav-tabs nav-list-y-2 mb-4">
+            <li class="nav-item">
+                <a class="nav-link @@if(link=='account-orders.html'){active}" href="account-orders.html">
+                <i class="fas fa-shopping-basket nav-icon"></i> Your Company
+                </a>
+            </li>
+        </ul>
+        <!-- End List -->
+
+        <h6 class="text-cap small">Payment Information</h6>
+
+        <!-- List -->
+        <ul class="nav nav-sub nav-sm nav-tabs nav-list-y-2">
+        <li class="nav-item">
+            <a class="nav-link @@if(link=='account-billing.html'){active}" href="account-billing.html">
+            <i class="fas fa-credit-card nav-icon"></i> Payment Method
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link @@if(link=='account-address.html'){active}" href="account-address.html">
+            <i class="fas fa-map-marker-alt nav-icon"></i> Payment Details
+            </a>
+        </li>
+        </ul>
+        <!-- End List -->
+
+        <div class="d-lg-none">
+        <div class="dropdown-divider"></div>
+
+        <!-- List -->
+        <ul class="nav nav-sub nav-sm nav-tabs nav-list-y-2">
+            <li class="nav-item">
+            <a class="nav-link text-primary" href="#">
+                <i class="fas fa-sign-out-alt nav-icon"></i> Log out
+            </a>
+            </li>
+        </ul>
+        <!-- End List -->
+        </div>
+                        <!-- End Nav -->
+                        </div>
+                    </div>
+                    <!-- End Card -->
+                    </div>
+                </div>
+                <!-- End Navbar -->
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                isActive:"the-basic-info"
+            }
+        },
+        methods:{
+
+            changeActive(newActive){
+                this.isActive = newActive;
+                this.$emit('change-active', newActive);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .nav-link{
+        cursor: pointer;
+    }
+</style>
---
+++ src/components/employer/account/personal-info/TheBasicInfo.vue
@@ -0,0 +1,192 @@
+<template>
+     <!-- Card -->
+    <div class="card mb-3 mb-lg-5">
+        <div class="card-header">
+        <h5 class="card-title">Basic info</h5>
+        </div>
+
+        <!-- Body -->
+        <div class="card-body">
+        <form>
+            <!-- Form Group -->
+            <div class="row form-group">
+            <label for="firstNameLabel" class="col-sm-3 col-form-label input-label">Full name <i class="far fa-question-circle text-body ml-1" data-toggle="tooltip" data-placement="top" title="Displayed on public forums, such as Front."></i></label>
+
+            <div class="col-sm-9">
+                <div class="input-group">
+                <input type="text" class="form-control" name="firstName" id="firstNameLabel" aria-label="First Name" v-model="firstname">
+                <input type="text" class="form-control" name="lastName" id="lastNameLabel" aria-label="Last Name" v-model="lastname">
+                </div>
+            </div>
+            </div>
+            <!-- End Form Group -->
+
+            <!-- Form Group -->
+            <div class="row form-group">
+                <label for="emailLabel" class="col-sm-3 col-form-label input-label">Email</label>
+
+                <div class="col-sm-9">
+                    <input type="email" class="form-control" name="email" id="emailLabel" aria-label="Email" v-model="email">
+                </div>
+            </div>
+            <!-- End Form Group -->
+
+            <!-- Form Group -->
+            <div class="js-add-field row form-group">
+                <label for="phoneLabel" class="col-sm-3 col-form-label input-label">Phone <span class="input-label-secondary">(Optional)</span></label>
+
+                <div class="col">
+                
+                    <div class="phone-container">
+                        <div class="input-group align-items-center">
+                            <input type="text" class="js-masked-input form-control" name="phone" id="phoneLabel" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx" v-model="mobile"
+                                    data-hs-mask-options='{
+                                    "template": "+0(000)000-00-00"
+                                    }'>
+
+                            
+                            <div class="input-group-append">
+
+                                <span class="phone-type" name="phoneSelect">Mobile</span>
+                        
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="phone-container">
+                        <div class="input-group align-items-center">
+                            <input type="text" class="js-masked-input form-control" name="phone" id="phoneLabel" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx" v-model="work"
+                                    data-hs-mask-options='{
+                                    "template": "+0(000)000-00-00"
+                                    }'>
+
+                            
+                            <div class="input-group-append">
+
+                                <span class="phone-type" name="phoneSelect">Work</span>
+                        
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                
+
+            </div>
+            <!-- End Form Group -->
+
+            <div class="row form-group" >
+                    <label for="role" class="col-sm-3 col-form-label input-label">Role</label>
+
+                    <div class="col-sm-9">
+
+                        <input v-model.lazy="role" type="text" class="form-control" name="role" id="role" aria-label="Role">
+                    </div>
+            </div>
+        </form>
+        </div>
+        <!-- End Body -->
+
+        <!-- Footer -->
+        <div class="card-footer d-flex justify-content-end">
+        <a class="btn btn-white" @click="cancel">Cancel</a>
+        <span class="mx-2"></span>
+        <a class="btn btn-primary" href="javascript:;">Save Changes</a>
+        </div>
+        <!-- End Footer -->
+    </div>
+    <!-- End Card -->
+</template>
+
+<script>
+    export default {
+        
+       async created(){
+            this.token = this.$cookies.get('com.bitjobs');
+          
+            this.employer = await fetch(process.env.VUE_APP_BIT_API_PATH + "/employer/get",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + this.token
+                    }
+                }
+                ).then(result =>{
+
+                    if(!result.ok){
+                        console.log(result)
+                        return result
+                    }
+                    return result.json()
+
+                })
+
+            if(this.employer){ 
+                this.firstname = this.employer.firstname;
+                this.lastname = this.employer.lastname;
+                this.email = this.employer.email;
+                this.mobile = this.employer.mobilenumber;
+                this.work = this.employer.phonenumber;
+                this.role = this.employer.role;
+            }
+
+       },
+
+        data(){
+            return  {
+                employer:{},
+                token:"",
+                firstname:"",
+                lastname:"",
+                email:"",
+                mobile:"",
+                work:"",
+                role:""
+            }
+        },
+
+        methods:{
+            cancel(){
+                this.firstname = this.employer.firstname;
+                this.lastname = this.employer.lastname;
+                this.email = this.employer.email;
+                this.mobile = this.employer.mobilenumber;
+                this.work = this.employer.phonenumber;
+                this.role = this.employer.role;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .phone-type{ 
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+        border-top-right-radius: 0.3125rem;
+        border-bottom-right-radius: 0.3125rem;
+        border: 0.0625rem solid #e7eaf3;
+        background-color: 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+        background: #fff no-repeat right 0.5rem center/1rem 1rem;
+        display: inline-block;
+        width:110px;
+        height: calc(1.6em + 1.625rem);
+        padding: 0.75rem 2rem 0.75rem 1rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.6;
+        color: #1e2022;
+        vertical-align: middle;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        box-sizing: border-box;
+    }
+
+    .phone-container{ 
+        margin-top:5px;
+    }
+</style>
